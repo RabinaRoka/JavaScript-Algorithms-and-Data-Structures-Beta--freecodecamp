017b0d3d7b602ab7ba3dcc645cb8e99a6d4bbe51
RabinaRokaa <rabinaroka112@gmail.com>
2024-08-08 16:57:05 +0545

    push and pop method for adding and removing

---
+++ Learn Introductory Javascript By building Pyramid Generator/29.js
@@ -0,0 +1,9 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let popped = rows.pop()
+let pushed = rows.push("freeCodeCamp","hi");
+console.log(pushed);
+
+console.log(popped);
+console.log(rows);
